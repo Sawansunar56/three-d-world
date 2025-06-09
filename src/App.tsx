97cd86000ae5f08ae14cfb69c3a3d9ae108201c1
Sawan Sunar <sawansunar24072002@gmail.com>
2025-06-09 21:48:08 +0000

    feat: some more dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import ThreeDSection from "./pages/model";
 import Blob from "./assets/actual_hover.svg?react";
 
 function App() {
+  const something = [
+    // "fonts/just_font.svg",
+    "fonts/move_font.svg",
+    "fonts/fair_font.svg",
+    "fonts/white_font.svg"
+  ]
   return (
     <div className="App">
       <ThreeDSection />
@@ -11,19 +17,12 @@ function App() {
           Hello there
           <span className="vibe"></span>
         </button>
-        <button>
-          <img src="fonts/just_font.svg" />
-          <Blob className="vibe" />
-        </button>
-        <button>
-          <img src="fonts/move_font.svg" />
-        </button>
-        <button>
-          <img src="fonts/fair_font.svg" />
-        </button>
-        <button>
-          <img src="fonts/white_font.svg" />
-        </button>
+        {something.map((item, index) => (
+          <button key={index}>
+            <img src={item} />
+            <Blob className="vibe" />
+          </button>
+        ))}
       </div>
     </div>
   );
